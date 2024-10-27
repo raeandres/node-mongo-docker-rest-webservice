ba2f1590c5e6bf8fe3acf8c007c3f46eca2e134c
Rae-An Andres <raeandres@users.noreply.github.com>
2024-10-27 19:54:30 +0800

    Added sample routes

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
- 
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+
+// Test Routes
+// Get route with sample return
 app.get('/api/courses', (req,res) => {
     const courses = ['english', 'math', 'science'];
     res.send(courses);
 });
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+// Get route with returning the user input param
+app.get('/api/posts/:year/:month', (req,res) => {
+    res.send(req.params);
 });
+
