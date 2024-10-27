6e91bffecad57878d7ebe9834733b7d1e109d959
Rae-An Andres <raeandres@users.noreply.github.com>
2024-10-27 19:50:55 +0800

    Updated package.json
    updated npm start script

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from "express";
 const app = express();
 
+const port = process.env.PORT || 4000;
+
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+ 
+app.get('/api/courses', (req,res) => {
+    const courses = ['english', 'math', 'science'];
+    res.send(courses);
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
