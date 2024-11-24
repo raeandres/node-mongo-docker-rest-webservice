57aa58d92a1a6944b3aa17a5b88c18234da34bb3
Rae-An Andres <raeandres@users.noreply.github.com>
2024-11-24 19:38:48 +0800

    Updated ConnectionString
    Updated connectionstring in db.js to take from env file

--- db.js
+++ db.js
@@ -2,9 +2,8 @@ import mongoose from "mongoose";
 
 
 const connectDB = async () => {
-  const mongoURI = "mongodb://root:password@db:27017/";
   try {
-    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
